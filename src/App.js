a132fe91911c7b066dc6d7dadd704958d6f3c48b
TomassSimko <simko.t@email.cz>
2022-08-12 19:08:46 +0200

    does not compile need to work on props to send and design

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
+import Register from "./components/Register";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <Register/>
       </header>
     </div>
   );
